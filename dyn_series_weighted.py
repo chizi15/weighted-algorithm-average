7b922ab766eb2f4daf45dbe379ef7f9191470f96
Chi Zhang <41175891+chizi15@users.noreply.github.com>
2020-07-13 09:28:47 +0800

    Add files via upload

---
+++ dyn_series_weighted.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+import random
+
+
+def dyn_df_weighted(df, type, w=None, r=3/2, d=1, initial=2):
+    """
+    传入df，根据df的列数动态计算基于几何级数或算数级数再作归一化的权重，将df各列与权重相乘再相加，得到一条最终序列。
+    :param df: 需要加权的各列组成的df
+    :param type: 采用几何级数或算数级数加权，type = 'geometric'或'arithmetic'
+    :param w: 权重系数可人为指定，默认为None
+    :param r: 指定几何级数分母的公比
+    :param d: 指定算数级数分母的公差
+    :param initial: 指定算数级数分母的初始值
+    :return: 将df各列分别乘以权重w再相加得到一条最终的加权序列
+    """
+    if w is None:
+        w = list()
+        if type == 'geometric':
+            for i in range(len(df.columns)):
+                w.append((1 / r) ** i)
+        elif type == 'arithmetic':
+            for i in range(len(df.columns)):
+                w.append(1 / (initial + d * i))
+        else:
+            raise Exception('if w=None, type must be one of geometric or arithmetic')
+        w = np.array(w) / sum(w)
+    elif (w is not None) and (len(w) == len(df.columns)):
+        w = np.array(w) / sum(w)
+    else:
+        raise Exception('手动输入的权重长度必须和一维数组长度相等')
+    print(w, sum(w))
+    return np.matmul(df.values, w)
+
+
+def dyn_seri_weighted(seri, type, w=None, r=3/2, d=1, initial=2):
+    """
+    传入seri，根据seri的长度动态计算基于几何级数或算数级数再作归一化的权重，将seri各点与权重相乘再相加，得到一条最终seri。
+    :param seri: 需要加权的一维数组
+    :param type: 采用几何级数或算数级数加权，type = 'geometric'或'arithmetic'
+    :param w: 权重系数可人为指定，默认为None
+    :param r: 指定几何级数分母的公比
+    :param d: 指定算数级数分母的公差
+    :param initial: 指定算数级数分母的初始值
+    :return: 返回seri各点与权重w相乘再相加，得到的一条最终加权序列
+    """
+    if w is None:
+        w = list()
+        if type == 'geometric':
+            for i in range(len(seri)):
+                w.append((1 / r) ** i)
+        elif type == 'arithmetic':
+            for i in range(len(seri)):
+                w.append(1 / (initial + d * i))
+        else:
+            raise Exception('if w=None, type must be one of geometric or arithmetic')
+        w = np.array(w) / sum(w)
+    elif (w is not None) and (len(w) == len(seri)):
+        w = np.array(w) / sum(w)
+    else:
+        raise Exception('手动输入的权重长度必须和一维数组长度相等')
+    print(w, sum(w))
+    return np.dot(seri, w)
+
+
+k_mul = np.array(range(7))
+y_all = [[]] * len(k_mul)
+
+for i in range(0, len(k_mul)):
+    y_all[i] = np.sin(random.choices(range(0, 100), k=20))
+
+y_all = pd.DataFrame(y_all).T
+y_all.fillna(value=0, inplace=True)
+
+print(dyn_df_weighted(y_all, type='geometric'), '\n')
+print(dyn_seri_weighted(y_all.loc[20-1, :], type='geometric'), '\n')
+print(dyn_df_weighted(y_all, type='arithmetic'), '\n')
+print(dyn_seri_weighted(y_all.loc[20-1, :], type='arithmetic'), '\n')
+print(dyn_df_weighted(y_all, w=np.ones(len(k_mul)), type='arithmetic'), '\n')
+print(dyn_seri_weighted(y_all.loc[20-1, :], w=np.ones(len(k_mul)), type='arithmetic'), '\n')
